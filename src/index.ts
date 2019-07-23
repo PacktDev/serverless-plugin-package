1702b2a05cb2caaae93cddc3621d851ce98777ba
James Macmillan <jamescodesthings@gmail.com>
2019-07-23 15:14:04 +0100

    Add basic implementation of the plugin
    Update `tsconfig.json` to output to `lib` folder
    Add `lib` folder to `.gitignore`
    Move `index.js` to `src/index.js`
    Add basic `package.json` tasks for `npm publish`
    Add `contributors`, `repository`, `keywords`, `types`, and `files` to `package.json`

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import Serverless from 'serverless';
+import semver from 'semver';
+
+const variablePrefix = 'package';
+const packageFile = '/package.json';
+
+export class Plugin {
+  private readonly serverless;
+  private readonly packageJson;
+
+  public constructor(serverless: Serverless) {
+    this.serverless = serverless;
+    this.packageJson = require(`${process.cwd()}/${packageFile}`);
+
+    this.bindVariableResolution();
+  }
+
+  private bindVariableResolution(): void {
+    const delegate = this.serverless.variables.getValueFromSource.bind(this.serverless.variables);
+
+    this.serverless.variables.getValueFromSource = (variable): string => {
+      if (variable.startsWith(`${variablePrefix}:`)) {
+        return this.resolveVariable(variable);
+      }
+
+      return delegate(variable);
+    };
+  }
+
+  private resolveVariable(variable: string): string {
+    const key = variable.split(`${variablePrefix}:`)[1];
+    this.serverless.cli.log(`Resolving ${key} from package.json`);
+
+    if (key.startsWith('semver.')) return this.resolveSemverSection(key);
+
+    return this.packageJson[key] || '';
+  }
+
+  private resolveSemverSection(key: string): string {
+    const { version } = this.packageJson;
+    const semverSection = key.split('semver.')[1];
+
+    if (!version) {
+      this.serverless.cli.log(
+        `Could not get ${key} from package.json, package.json is missing key 'version'`,
+      );
+      return '';
+    }
+
+    this.serverless.cli.log(`Resolving semver ${semverSection} from: ${version}`);
+    if (typeof semver[semverSection] === 'function') {
+      return semver[semverSection](version);
+    }
+
+    this.serverless.cli.log(`Could not get ${key}, ${semverSection} is not a function.`);
+    return '';
+  }
+}
+
+module.exports = Plugin;
